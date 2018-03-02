20aa5dac0a8ec456aa41f8833633c5d549deadce
Xinyun Liu <xinyunliu@gmail.com>
2018-03-02 22:04:40 +0800

    change name

--- jni/Main.java
+++ jni/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public native int square(int i);
 	public static void main(String[] args) {
-		System.loadLibrary("Main");
+		System.loadLibrary("mymath");
 		System.out.println(new Main().square(2));
 	}
 }
